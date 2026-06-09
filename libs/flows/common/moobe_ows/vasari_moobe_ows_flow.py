b7a73faa37d5a43c8f159c22e2ea0975ec71288a
Ansiyatk-coder <ansiya.thangal@ascendion.com>
2026-06-09 15:22:01 +0530

    Delete libs/flows/common directory

--- libs/flows/common/moobe_ows/vasari_moobe_ows_flow.py
+++
@@ -1,5 +0,0 @@
-from MobileApps.libs.flows.common.moobe_ows.moobe_ows_flow_container import Gen2MoobeOWSFlowContainer
-
-
-class VasariMoobeOWSFlow(Gen2MoobeOWSFlowContainer):
-    project = "vasari"
