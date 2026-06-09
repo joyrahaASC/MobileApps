7e3e655d21389e7a1ae91bcd028f05ab656067e8
Ansiyatk-coder <ansiya.thangal@ascendion.com>
2026-06-09 15:24:39 +0530

    Delete libs/flows/windows/jweb_value_store directory

--- libs/flows/windows/jweb_value_store/jweb_value_store_flow.py
+++
@@ -1,6 +0,0 @@
-from abc import ABCMeta
-from MobileApps.libs.flows.windows.windows_flow import WindowsFlow
-
-class JwebValueStoreFlow(WindowsFlow):
-    __metaclass__ = ABCMeta
-    project = "jweb_value_store"
