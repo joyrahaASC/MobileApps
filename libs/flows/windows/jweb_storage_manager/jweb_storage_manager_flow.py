47bd565475c61a1190ba85e738d1c74646999aec
Ansiyatk-coder <ansiya.thangal@ascendion.com>
2026-06-09 15:25:43 +0530

    Delete libs/flows/windows/jweb_storage_manager/jweb_storage_manager_flow.py

--- libs/flows/windows/jweb_storage_manager/jweb_storage_manager_flow.py
+++
@@ -1,6 +0,0 @@
-from abc import ABCMeta
-from MobileApps.libs.flows.windows.windows_flow import WindowsFlow
-
-class JwebStorageManagerFlow(WindowsFlow):
-    __metaclass__ = ABCMeta
-    project = "jweb_storage_manager"
