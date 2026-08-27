282a78cc26469c83ecae7730a4b9bed5285f2985
joyrahaASC <joy.raha@ascendion.com>
2026-08-26 22:41:17 -0500

    Workflow-NF: Appended methods in libs/flows/windows/hpx_rebranding/profile.py

--- libs/flows/windows/hpx_rebranding/profile.py
+++ libs/flows/windows/hpx_rebranding/profile.py
@@ -7,4 +7,25 @@ class Profile(HPXRebrandingFlow):
     def click_sign_in_from_avatar_sideflyout(self):
         self.driver.wait_for_object("sideflyout_sign_in_link", timeout=20)
         self.driver.click("sideflyout_sign_in_link")
-        
+
+    def verify_profile_icon_visible(self):
+        try:
+            self.driver.wait_for_object("profile_icon_signed_in", timeout=10)
+            return True
+        except:
+            return False
+
+    def click_profile_button(self):
+        self.driver.wait_for_object("profile_icon_signed_in", timeout=20)
+        self.driver.click("profile_icon_signed_in", timeout=10)
+
+    def verify_feedback_button_present(self):
+        try:
+            self.driver.wait_for_object("feedback_btn", timeout=10, raise_e=False)
+            return True
+        except:
+            return False
+
+    def click_feedback_button(self):
+        self.driver.wait_for_object("feedback_btn", timeout=10)
+        self.driver.click("feedback_btn", timeout=10)
