002a6b82ea4e6ef044dfc3127690d0eb593cee4a
joyrahaASC <joy.raha@ascendion.com>
2026-08-27 23:26:40 -0500

    Agent-5: Native Flow Container Mutation

--- libs/flows/windows/hpx_rebranding/flow_container.py
+++ libs/flows/windows/hpx_rebranding/flow_container.py
@@ -76,12 +76,15 @@ from MobileApps.libs.flows.windows.hpx_rebranding.add_device import AddDevice
 from MobileApps.libs.ma_misc.live_printer import LivePrinter
 from selenium.webdriver.common.keys import Keys
 
+from MobileApps.libs.flows.windows.hpx_rebranding.profile_panel import ProfilePanel
+
 class FlowContainer(object):
     def __init__(self, driver):
         self.driver = driver
         # Cache for MFE locale files to avoid repeated network calls
         self._mfe_locale_cache = {}
         self.fd = {
+            "profilePanel": ProfilePanel(driver),
                    "devicesMFE": DevicesMFE(driver),
                    "devicesDetailsMFE": DevicesDetailsPrinterMFE(driver),
                    "scan": Scan(driver),
